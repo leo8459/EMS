ecf810416bdf43986811478d12602ac2946e1718
Leo <joseaguilar987654321@gmail.com>
2025-02-18 17:31:31 -0400

    Update ApiController.php

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -72,7 +72,7 @@ public function cambiarEstadoPorCodigoEMS(Request $request)
         'accion' => 'Asignar Cartero',
         'descripcion' => "Envio En camino a ser entregado ",
         'codigo' => $admision->codigo,
-        // 'user_id' => Auth::id(),
+        'user_id' => Auth::name(),
     ]);
     Historico::create([
         'numero_guia' => $admision->codigo, // Asignar el código único de admisión al número de guía
