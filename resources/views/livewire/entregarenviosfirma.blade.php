70dd4dd64baa8217fed998a5090e0879b2db3a96
Leo <joseaguilar987654321@gmail.com>
2026-01-20 19:15:26 -0400

    Update entregarenviosfirma.blade.php

--- resources/views/livewire/entregarenviosfirma.blade.php
+++ resources/views/livewire/entregarenviosfirma.blade.php
@@ -115,12 +115,11 @@
                                     @endhasrole
 
                                     <div class="col-md-3">
-                                        <label for="photo">Seleccionar Foto (máx. 50MB)</label>
+                                        <label for="photo">Seleccionar Foto (máx. 50MB) - Cámara o Galería</label>
                                         <input type="file"
                                                id="photo"
                                                wire:model="photo"
                                                accept="image/*"
-                                               capture="environment"
                                                max="52428800"
                                                class="form-control">
                                         <small class="text-muted">Si falla la subida, intenta con una foto de menor resolución o desde la galería.</small>
